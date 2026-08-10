3e739120c63ca5c469adb76e3d36e1d9cc3981c3
chacha86 <taejincha0619@gmail.com>
2026-08-10 11:41:24 +0900

    9

--- src/main/java/com/back/p65260807/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/p65260807/domain/post/post/controller/PostController.java
@@ -2,14 +2,18 @@ package com.back.p65260807.domain.post.post.controller;
 
 import com.back.p65260807.domain.post.post.entity.Post;
 import com.back.p65260807.domain.post.post.service.PostService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 public class PostController {
 
     private final PostService postService;
@@ -23,10 +27,10 @@ public class PostController {
 
     @PostMapping("/posts/doWrite")
     @ResponseBody
-    public String doWrite(String title, String content) {
-
-        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.", title, content, "title");
-        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.", title, content, "content");
+    public String doWrite(
+            @NotBlank @Size(min=2, max=10) String title,
+            @NotBlank @Size(min=2, max=10) String content
+    ) {
 
         Post post = postService.write(title, content);
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
