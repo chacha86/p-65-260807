28d2bbb668bcf4f396e3f78fac1ba92fbba3a4ec
chacha86 <taejincha0619@gmail.com>
2026-08-10 10:21:04 +0900

    5

--- src/main/java/com/back/p65260807/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/p65260807/domain/post/post/controller/PostController.java
@@ -1,18 +1,24 @@
 package com.back.p65260807.domain.post.post.controller;
 
+import com.back.p65260807.domain.post.post.entity.Post;
+import com.back.p65260807.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
 
     @GetMapping("/posts/write")
     @ResponseBody
     public String write() {
 
         return """
-                <form action="https://localhost:8080/">
+                <form action="/posts/doWrite">
                   <input type="text" name="title">
                   <br>
                   <textarea name="content"></textarea>
@@ -20,6 +26,14 @@ public class PostController {
                   <input type="submit" value="작성">
                 </form>
                 """;
+    }
+
+    @GetMapping("/posts/doWrite")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+
+        Post post = postService.write(title, content);
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 
 }
